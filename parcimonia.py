8c434b891908f17f24b83ac47d3ae616cb80fe93
Tommy <79540750+TBDwarf@users.noreply.github.com>
2025-11-05 22:55:13 +0100

    Update parcimonia.py

--- parcimonia.py
+++ parcimonia.py
@@ -18,7 +18,7 @@ import numpy as np
 # tout en maintenant la qualité perçue.
 #
 # Version 1.0
-# Copyright (c) 2025 TBDwarf - Tommy RENAULT
+# Copyright (c) 2025 - TBDwarf
 # License: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
